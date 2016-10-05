532bbde2c4412a6534334c3cc16f3150816b7f1d
Fabricio <fabriciolopesvital@gmail.com>
2016-10-04 21:44:30 -0300

    remove comments and add rvm install

--- osx_install.sh
+++ osx_install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+#install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 brew tap Homebrew/bundle
 brew bundle
 
-# sudo pip install -q -r pip_requirements.txt
-
-# mkdir -p ~/.virtualenvs
-
+#install xcode
 xcode-select --install
 
+#install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#install rvm/ruby/rails
+\curl -sSL https://get.rvm.io | bash -s stable --rails
